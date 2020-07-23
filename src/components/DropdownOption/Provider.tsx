53c3c2ae8dc3183918504660c2a67cce25e208e0
Eduardo SO <eduardo.sool07@gmail.com>
2020-07-22 22:08:19 -0300

    add: Dropdown provider

---
+++ src/components/DropdownOption/Provider.tsx
@@ -0,0 +1,81 @@
+import React, { useState, createContext, useCallback, useEffect } from 'react';
+
+interface Item {
+  id: number;
+  optionDimensions: number;
+  optionCenterX: number;
+  WrappedContent: number;
+  backgroundHeight: number;
+}
+
+const Context = createContext({});
+
+export const DropdownProvider: React.FC = ({ children }) => {
+  const [options, setOptions] = useState<Item[]>([]);
+  const [targetId, setTargetId] = useState(null);
+  const [cachedId, setCachedId] = useState(null);
+
+  const registerOption = useCallback(
+    ({
+      id,
+      optionDimensions,
+      optionCenterX,
+      WrappedContent,
+      backgroundHeight,
+    }) => {
+      setOptions(items => [
+        ...items,
+        {
+          id,
+          optionDimensions,
+          optionCenterX,
+          WrappedContent,
+          backgroundHeight,
+        },
+      ]);
+    },
+    [setOptions],
+  );
+
+  const updateOptionProps = useCallback(
+    (optionId, props) => {
+      setOptions(items =>
+        items.map(item => {
+          if (item.id === optionId) {
+            item = { ...item, ...props };
+          }
+
+          return item;
+        }),
+      );
+    },
+    [setOptions],
+  );
+
+  const getOptionById = useCallback(
+    id => options.find(item => item.id === id),
+    [options],
+  );
+
+  const deleteOptionById = useCallback(
+    id => setOptions(items => items.filter(item => item.id !== id)),
+    [setOptions],
+  );
+
+  useEffect(() => {
+    if (targetId !== null) setCachedId(targetId);
+  }, [targetId]);
+
+  return (
+    <Context.Provider
+      value={{
+        registerOption,
+        updateOptionProps,
+        getOptionById,
+        deleteOptionById,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 
 import { Company, Developers, Products } from '../Content';
-import { DropdownOption } from '../DropdownOption';
+import { DropdownOption, DropdownProvider } from '../DropdownOption';
 
 import { Container, DropdownStyles } from './styles';
 
 const NavBar: React.FC = () => {
   return (
-    <DropdownStyles>
-      <Container>
-        <ul>
-          <li>
-            <DropdownOption name="Produtos" content={Company} />
-          </li>
-          <li>
-            <DropdownOption name="Desenvolvedores" content={Developers} />
-          </li>
-          <li>
-            <DropdownOption name="Empresa" content={Products} />
-          </li>
-        </ul>
-      </Container>
-    </DropdownStyles>
+    <DropdownProvider>
+      <DropdownStyles>
+        <Container>
+          <ul>
+            <li>
+              <DropdownOption name="Produtos" content={Company} />
+            </li>
+            <li>
+              <DropdownOption name="Desenvolvedores" content={Developers} />
+            </li>
+            <li>
+              <DropdownOption name="Empresa" content={Products} />
+            </li>
+          </ul>
+        </Container>
+      </DropdownStyles>
+    </DropdownProvider>
   );
 };
 
