e03013de193ed2e2f7a27e2e41ae270f6da178f8
Eduardo SO <eduardo.sool07@gmail.com>
2020-07-24 23:27:49 -0300

    Dimensions hook

---
+++ src/components/DropdownOption/dimensions.tsx
@@ -0,0 +1,33 @@
+import { useState, useCallback, useLayoutEffect } from 'react';
+
+const getDimensions = (element: Element): ClientRect =>
+  element.getBoundingClientRect();
+
+export const useDimensions = (
+  responsive: boolean,
+): (Element | ClientRect | ((e: Element) => void) | null)[] => {
+  const [dimensions, setDimensions] = useState<ClientRect | null>(null);
+  const [element, setElement] = useState<Element | null>(null);
+
+  const hook = useCallback((e: Element) => setElement(e), []);
+
+  useLayoutEffect(() => {
+    if (element) {
+      const updateDimensions = (): void => {
+        window.requestAnimationFrame(() => {
+          setDimensions(getDimensions(element));
+        });
+      };
+
+      updateDimensions();
+
+      if (responsive) {
+        window.addEventListener('resize', updateDimensions);
+
+        window.removeEventListener('resize', updateDimensions);
+      }
+    }
+  }, [element, responsive]);
+
+  return [hook, dimensions, element];
+};
