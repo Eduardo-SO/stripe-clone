4854d8bab9a13f852d7d2433d23cb88877f35ad4
Eduardo SO <eduardo.sool07@gmail.com>
2020-07-30 22:41:18 -0300

    add: Dropdown root, container and section initial style

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -28,4 +28,20 @@ export const DropdownStyles = styled.div`
       opacity: 0.55;
     }
   }
+
+  .dropdown-root {
+    z-index: 10;
+    position: absolute;
+  }
+
+  .dropdown-arrow {
+  }
+
+  .dropdown-container {
+    position: absolute;
+  }
+
+  .dropdown-section {
+    position: absolute;
+  }
 `;
